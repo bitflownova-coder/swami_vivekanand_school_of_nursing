11cf80b77a2e89c9a4f93a8b2ec71781dfa756c1
kunalg12 <kunalgursal2923@gmail.com>
2025-11-25 15:19:49 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: { unoptimized: true },
+  // ESLint block removed to fix the warning
 };
 
 module.exports = nextConfig;
