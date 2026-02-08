0063ac775919c89202c55fa258b1a7f79cfb155e
Sai Care Group <admin@saicaregroup.com>
2026-02-08 14:34:44 +0530

    Remove migration file after applying to production

--- database/add-attendance-qr-token.sql
+++
@@ -1,5 +0,0 @@
--- Add attendanceQRToken field to workshops table
--- This allows each workshop to have a permanent attendance QR code
-
-ALTER TABLE `workshops` 
-ADD COLUMN `attendanceQRToken` VARCHAR(500) NOT NULL DEFAULT '' AFTER `spotRegistrationQRToken`;
